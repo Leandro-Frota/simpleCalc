88a784f723021b9c00558cbf7ebc57cf6e06496c
Leandro-Frota <leandromfrota@gmail.com>
2024-01-17 17:03:16 -0300

    Alterei o icone darkLightMood para select

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,7 +1,9 @@
 let themeButton = document.getElementById('themeButton');
 let iconTheme = document.getElementById('iconTheme');
 let themeButtonRight = document.getElementById('themeButtonRight')
-let iconTheme2 = document.getElementById('iconTheme2')
+// let iconTheme2 = document.getElementById('iconTheme2')
+let selectDarkLight = document.getElementById('selectDarkLight')
+let valueSelect = selectDarkLight.options
 let sideRigth = document.getElementById('sideRigth')
 
 let body = document.querySelector('body');
@@ -14,13 +16,24 @@ themeButton.addEventListener('click', function () {
     iconTheme.classList.toggle('fa-sun');
 });
 
-themeButtonRight.addEventListener('click',function(){
-    sideRigth.classList.toggle('yelow')
-    sideRigth.classList.toggle('purple')
+selectDarkLight.addEventListener('change',function(){
 
-    iconTheme2.classList.toggle('fa-moon');
-    iconTheme2.classList.toggle('fa-sun');
+console.log(selectDarkLight.value)
+
+    if(selectDarkLight.value== 'dark'){
+        sideRigth.classList.remove('yellow')
+        sideRigth.classList.add('purple')
+    }else if(selectDarkLight.value== 'ligth'){
+        sideRigth.classList.remove('purple')
+        sideRigth.classList.add('yellow')
+    }
+    // sideRigth.classList.toggle('yelow')
+    // sideRigth.classList.toggle('purple')
+
+    // iconTheme2.classList.toggle('fa-moon');
+    // iconTheme2.classList.toggle('fa-sun');
 })
+
 
 
 
